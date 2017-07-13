25f5a6f015475c8a7c2d3a796c8664a583cce753
AmberXiong <pxiong@lbl.gov>
2017-07-13 06:36:21 -0700

    Take an average every 8000 points, and set loops to 500.

--- RFSampAcy.py
+++ RFSampAcy.py
@@ -20,7 +20,7 @@ import seaborn as sns
 def Simple_DFT(x, fft_size):
     xs = x[:fft_size]              # reshape input array with fft_size
     xf = np.fft.rfft(xs)/fft_size  # DFT
-    xps = 2*abs(xf)*abs(xf)
+    xps = 2*abs(xf)*abs(xf)/50
 
     if fft_size%2:                 # fft_size is even
         xps[0] /= 2
@@ -28,7 +28,7 @@ def Simple_DFT(x, fft_size):
         xps[0] /= 2
         xps[int(fft_size/2)] /= 2
     xps_dBm = 10*np.log10(xps*1000)
-    return [xps_dBm, xps]
+    return [xps_dBm, numpy.sqrt(xps)]
     #return xf
 
 ## Multiply window function to array x.
@@ -276,7 +276,7 @@ def Plot_PS_W(xps1, xps1_w, xps2, xps2_w, freqs, signal_amp, sampling_rate, fft_
 
 if __name__ == "__main__":
     sampling_rate = 400
-    fft_size      = 1024
+    fft_size      = 512
     signal_freq   = 130
     signal_amp    = 1
     wht_mean      = 0
@@ -286,9 +286,10 @@ if __name__ == "__main__":
     v_fs          = 2
     width         = 10
     Q             = v_fs/numpy.power(2, width)
-    mean_num      = 20  # Take an average of every mean_num numbers
-    times         = 200  # number of results
-    loops         = mean_num*times  # number of loops
+    #mean_num      = 20  # Take an average of every mean_num numbers
+    #times         = 200  # number of results
+    window_num    = 8000  # number of window
+    loops         = 500
     lenth         = (fft_size+2)//2  # data lenth of FFT results 
     num_bins      = 50  # bin number for full frequency scale
     bins          = 10  # bin number for signal amplitude
@@ -298,117 +299,191 @@ if __name__ == "__main__":
     freqs = np.linspace(0, sampling_rate/2, fft_size/2+1)
     w     = 2*np.pi*signal_freq*t
 
-    Y          = []
-    Y_wht      = []
-    Y_phs      = []
-    Y_qnt      = []
-    Y_all      = []
-    Y_w        = []
-    Y_wht_w    = []
-    Y_phs_w    = []
-    Y_qnt_w    = []
-    Y_all_w    = []
+    Avrg_Y     = []
+    Avrg_Y_wht = []
+    Avrg_Y_phs = []
+    Avrg_Y_qnt = []
+    Avrg_Y_all = []
 
-    Y_dB       = []
-    Y_wht_dB   = []
-    Y_phs_dB   = []
-    Y_qnt_dB   = []
-    Y_all_dB   = []
-    Y_w_dB     = []
-    Y_wht_w_dB = []
-    Y_phs_w_dB = []
-    Y_qnt_w_dB = []
-    Y_all_w_dB = []
-    # repeat m times, for white noise and phase noise
-    for i in range(loops):
-        x     = signal_amp*np.sin(w)  #sample the points near the +-1
-        x_wht = Add_White_Noise(x, wht_mean, wht_std, fft_size)
-        w_phs = Add_Phase_Noise(w, phs_mean, phs_std, fft_size)
-        x_phs = signal_amp*np.sin(w_phs)
+    Avrg_Y_dB     = []
+    Avrg_Y_wht_dB = []
+    Avrg_Y_phs_dB = []
+    Avrg_Y_qnt_dB = []
+    Avrg_Y_all_dB = []
+    for n in range(loops):
+        #Y          = []
+        #Y_wht      = []
+        #Y_phs      = []
+        #Y_qnt      = []
+        #Y_all      = []
+        Y_w        = []
+        Y_wht_w    = []
+        Y_phs_w    = []
+        Y_qnt_w    = []
+        Y_all_w    = []
 
-        x_p   = signal_amp*np.sin(w+i*2*np.pi/loops)
-        x_qnt = Add_Qnt_Noise(x_p, v_fs, width, fft_size)
+        #Y_dB       = []
+        #Y_wht_dB   = []
+        #Y_phs_dB   = []
+        #Y_qnt_dB   = []
+        #Y_all_dB   = []
+        Y_w_dB     = []
+        Y_wht_w_dB = []
+        Y_phs_w_dB = []
+        Y_qnt_w_dB = []
+        Y_all_w_dB = []
+        # repeat m times, for white noise and phase noise
+        for i in range(window_num):
+            x     = signal_amp*np.sin(w)  #sample the points near the +-1
+            x_wht = Add_White_Noise(x, wht_mean, wht_std, fft_size)
+            w_phs = Add_Phase_Noise(w, phs_mean, phs_std, fft_size)
+            x_phs = signal_amp*np.sin(w_phs)
+
+            x_p   = signal_amp*np.sin(w+i*np.pi/window_num+n*np.pi/loops)
+            x_qnt = Add_Qnt_Noise(x_p, v_fs, width, fft_size)
+            
+            x_phs_wht = Add_White_Noise(signal_amp*np.sin(w_phs+i*np.pi/window_num+n*np.pi/loops), wht_mean, wht_std, fft_size)
+            x_all = Add_Qnt_Noise(x_phs_wht, v_fs, width, fft_size) 
+            
+            #window  = signal.hamming(fft_size, sym=0)
+            window  = signal.hann(fft_size, sym=0)
+            #window  = np.array([1 for i in range(fft_size)])
+            #window  = signal.bartlett(fft_size, sym=0)
+
+            #x_w     = Apply_Window(x, window, fft_size)
+            #x_wht_w = Apply_Window(x_wht, window, fft_size)
+            #x_phs_w = Apply_Window(x_phs, window, fft_size)
+            x_qnt_w = Apply_Window(x_qnt, window, fft_size)
+            x_all_w = Apply_Window(x_all, window, fft_size)
+
+            #y       = Simple_DFT(x, fft_size)
+            #y_wht   = Simple_DFT(x_wht, fft_size)
+            #y_phs   = Simple_DFT(x_phs, fft_size)
+            #y_qnt   = Simple_DFT(x_qnt, fft_size)
+            #y_all   = Simple_DFT(x_all, fft_size)
+            #y_w     = Simple_DFT(x_w, fft_size)
+            #y_wht_w = Simple_DFT(x_wht_w, fft_size)
+            #y_phs_w = Simple_DFT(x_phs_w, fft_size)
+            y_qnt_w = Simple_DFT(x_qnt_w, fft_size)
+            y_all_w = Simple_DFT(x_all_w, fft_size)
+            
+            # power
+            #Y.append(y[1])
+            #Y_wht.append(y_wht[1])
+            #Y_phs.append(y_phs[1])
+            #Y_qnt.append(y_qnt[1])
+            #Y_all.append(y_all[1])
+
+            #Y_w.append(y_w[1])
+            #Y_wht_w.append(y_wht_w[1])
+            #Y_phs_w.append(y_phs_w[1])
+            Y_qnt_w.append(y_qnt_w[1])
+            Y_all_w.append(y_all_w[1])
+
+            freq_y = numpy.argmax(y_all_w[1])*sampling_rate/fft_size
+            
+            #Y_dB.append(y[0])
+            #Y_wht_dB.append(y_wht[0])
+            #Y_phs_dB.append(y_phs[0])
+            #Y_qnt_dB.append(y_qnt[0])
+            #Y_all_dB.append(y_all[0])
+
+            #Y_w_dB.append(y_w[0])
+            #Y_wht_w_dB.append(y_wht_w[0])
+            #Y_phs_w_dB.append(y_phs_w[0])
+            Y_qnt_w_dB.append(y_qnt_w[0])
+            Y_all_w_dB.append(y_all_w[0])
         
-        x_phs_wht = Add_White_Noise(signal_amp*np.sin(w_phs+i*2*np.pi/loops), wht_mean, wht_std, fft_size)
-        x_all = Add_Qnt_Noise(x_phs_wht, v_fs, width, fft_size) 
+        #Y_w_trans = numpy.transpose(Y_w)
+        #Y_wht_w_trans = numpy.transpose(Y_wht_w)
+        #Y_phs_w_trans = numpy.transpose(Y_phs_w)
+        Y_qnt_w_trans = numpy.transpose(Y_qnt_w)
+        Y_all_w_trans = numpy.transpose(Y_all_w)
+
+        #Y_w_dB_trans     = numpy.transpose(Y_w_dB)
+        #Y_wht_w_dB_trans = numpy.transpose(Y_wht_w_dB)
+        #Y_phs_w_dB_trans = numpy.transpose(Y_phs_w_dB)
+        Y_qnt_w_dB_trans = numpy.transpose(Y_qnt_w_dB)
+        Y_all_w_dB_trans = numpy.transpose(Y_all_w_dB)
+
+        #single_Y_w = []
+        #single_Y_wht_w = []
+        #single_Y_phs_w = []
+        single_Y_qnt_w = []
+        single_Y_all_w = []
+
+        #single_w_dB = []
+        #single_wht_w_dB = []
+        #single_phs_w_dB = []
+        single_qnt_w_dB = []
+        single_all_w_dB = []
+
+        for k in range(lenth):
+            #s_w     = numpy.mean(Y_w_trans[k])
+            #s_wht_w = numpy.mean(Y_wht_w_trans[k])
+            #s_phs_w = numpy.mean(Y_phs_w_trans[k])
+            s_qnt_w = numpy.mean(Y_qnt_w_trans[k])
+            s_all_w = numpy.mean(Y_all_w_trans[k])
+
+            #s_w_dB     = numpy.mean(Y_w_dB_trans[k])
+            #s_wht_w_dB = numpy.mean(Y_wht_w_dB_trans[k])
+            #s_phs_w_dB = numpy.mean(Y_phs_w_dB_trans[k])
+            s_qnt_w_dB = numpy.mean(Y_qnt_w_dB_trans[k])
+            s_all_w_dB = numpy.mean(Y_all_w_dB_trans[k])
+
+            #single_Y_w.append(s_w)
+            #single_Y_wht_w.append(s_wht_w)
+            #single_Y_phs_w.append(s_phs_w)
+            single_Y_qnt_w.append(s_qnt_w)
+            single_Y_all_w.append(s_all_w)
+
+            #single_w_dB.append(s_w_dB)
+            #single_wht_w_dB.append(s_wht_w_dB)
+            #single_phs_w_dB.append(s_phs_w_dB)
+            single_qnt_w_dB.append(s_qnt_w_dB)
+            single_all_w_dB.append(s_all_w_dB)
         
-        #window  = signal.hamming(fft_size, sym=0)
-        window  = signal.hann(fft_size, sym=0)
-        #window  = np.array([1 for i in range(fft_size)])
-        #window  = signal.bartlett(fft_size, sym=0)
+        print(len(single_Y_qnt_w)) 
+        #average amplitude for each frequency bin
+        #Avrg_Y.append(max(single_Y_w))
+        #Avrg_Y_wht.append(max(single_Y_wht_w))
+        #Avrg_Y_phs.append(max(single_Y_phs_w))
+        Avrg_Y_qnt.append(max(single_Y_qnt_w))
+        Avrg_Y_all.append(max(single_Y_all_w))
 
-        x_w     = Apply_Window(x, window, fft_size)
-        x_wht_w = Apply_Window(x_wht, window, fft_size)
-        x_phs_w = Apply_Window(x_phs, window, fft_size)
-        x_qnt_w = Apply_Window(x_qnt, window, fft_size)
-        x_all_w = Apply_Window(x_all, window, fft_size)
-
-        y       = Simple_DFT(x, fft_size)
-        y_wht   = Simple_DFT(x_wht, fft_size)
-        y_phs   = Simple_DFT(x_phs, fft_size)
-        y_qnt   = Simple_DFT(x_qnt, fft_size)
-        y_all   = Simple_DFT(x_all, fft_size)
-        y_w     = Simple_DFT(x_w, fft_size)
-        y_wht_w = Simple_DFT(x_wht_w, fft_size)
-        y_phs_w = Simple_DFT(x_phs_w, fft_size)
-        y_qnt_w = Simple_DFT(x_qnt_w, fft_size)
-        y_all_w = Simple_DFT(x_all_w, fft_size)
+        #Avrg_Y_dB.append(single_w_dB)
+        #Avrg_Y_wht_dB.append(single_wht_w_dB)
+        #Avrg_Y_phs_dB.append(single_phs_w_dB)
+        Avrg_Y_qnt_dB.append(single_qnt_w_dB)
+        Avrg_Y_all_dB.append(single_all_w_dB)
+    #A       = numpy.sqrt(Y)  #Amplitude of signal
+    #A_wht   = numpy.sqrt(Y_wht)
+    #A_wht_w = numpy.sqrt(Y_wht_w)
+    #A_qnt   = numpy.sqrt(Y_qnt)
+    #A_qnt_w = numpy.sqrt(Y_qnt_w)
+    #A_phs   = numpy.sqrt(Y_phs)
+    #A_phs_w = numpy.sqrt(Y_phs_w)
+    #A_all   = numpy.sqrt(Y_all)
+    #A_all_w = numpy.sqrt(Y_all_w)
         
-        # power
-        Y.append(max(y[1]))
-        Y_wht.append(max(y_wht[1]))
-        Y_phs.append(max(y_phs[1]))
-        Y_qnt.append(max(y_qnt[1]))
-        Y_all.append(max(y_all[1]))
-
-        Y_w.append(max(y_w[1]))
-        Y_wht_w.append(max(y_wht_w[1]))
-        Y_phs_w.append(max(y_phs_w[1]))
-        Y_qnt_w.append(max(y_qnt_w[1]))
-        Y_all_w.append(max(y_all_w[1]))
-
-        freq_y = numpy.argmax(y[1])*sampling_rate/fft_size
-        
-        Y_dB.append(y[0])
-        Y_wht_dB.append(y_wht[0])
-        Y_phs_dB.append(y_phs[0])
-        Y_qnt_dB.append(y_qnt[0])
-        Y_all_dB.append(y_all[0])
-
-        Y_w_dB.append(y_w[0])
-        Y_wht_w_dB.append(y_wht_w[0])
-        Y_phs_w_dB.append(y_phs_w[0])
-        Y_qnt_w_dB.append(y_qnt_w[0])
-        Y_all_w_dB.append(y_all_w[0])
-    
-    A       = numpy.sqrt(Y)  #Amplitude of signal
-    A_wht   = numpy.sqrt(Y_wht)
-    A_wht_w = numpy.sqrt(Y_wht_w)
-    A_qnt   = numpy.sqrt(Y_qnt)
-    A_qnt_w = numpy.sqrt(Y_qnt_w)
-    A_phs   = numpy.sqrt(Y_phs)
-    A_phs_w = numpy.sqrt(Y_phs_w)
-    A_all   = numpy.sqrt(Y_all)
-    A_all_w = numpy.sqrt(Y_all_w)
-    
     #Y_wht_w_mean = Avrg_Amp(A_wht_w, mean_num, times, bins, "Average_amp_dist_y_wht_w.csv")
     #Y_qnt_w_mean = Avrg_Amp(A_qnt_w, mean_num, times, bins, "Average_amp_dist_y_qnt_w.csv")
-    Y_all_w_mean = Avrg_Amp(A_all_w, mean_num, times, bins, "all_hann_160.csv")
+    #Y_all_w_mean = Avrg_Amp(A_all_w, mean_num, times, bins, "all_hann_160.csv")
     #Y_phs_w_mean = Avrg_Amp(A_phs_w, mean_num, times, bins, "Average_amp_dist_y_phs_w.csv")
 
     #D_wht_w = Data_Dist(Y_wht_w_dB, freqs, loops, lenth, num_bins, "RF_wht_window.csv")
-    #D_qnt_w = Data_Dist(Y_qnt_w_dB, freqs, loops, lenth, num_bins, "RF_qnt_window_dB.csv")
-    #D_all_w = Data_Dist(Y_all_w_dB, freqs, loops, lenth, num_bins, "RF_all_window_dB.csv")
+    D_qnt_w = Data_Dist(Y_qnt_w_dB, freqs, loops, lenth, num_bins, "RF_qnt_window_dB.csv")
+    D_all_w = Data_Dist(Avrg_Y_all_dB, freqs, loops, lenth, num_bins, "RF_all_window_dB.csv")
     #D_phs_w = Data_Dist(Y_phs_w_dB, freqs, loops, lenth, num_bins, "RF_phs_window_dB.csv")
 
-    #print("std_A_wht_w:",numpy.std(A_wht_w),"mean_A_wht_w:",numpy.mean(A_wht_w))
+    #print("std_A_wht_w:",numpy.std(Avrg_Y_wht),"mean_A_wht_w:",numpy.mean(Avrg_Y_wht))
     #print("std_A_wht_w_mean:",numpy.std(Y_wht_w_mean),"mean_A_wht_w_mean:",numpy.mean(Y_wht_w_mean))
-    #print("std_A_qnt_w:",numpy.std(A_qnt_w),"mean_A_qnt_w:",numpy.mean(A_qnt_w))
+    print("std_A_qnt_w:",numpy.std(Avrg_Y_qnt),"mean_A_qnt_w:",numpy.mean(Avrg_Y_qnt))
     #print("std_A_qnt_w_mean:",numpy.std(Y_qnt_w_mean),"mean_A_qnt_w_mean:",numpy.mean(Y_qnt_w_mean))
-    #print("std_A_phs_w:",numpy.std(A_phs_w),"mean_A_phs_w:",numpy.mean(A_phs_w))
+    #print("std_A_phs_w:",numpy.std(Avrg_Y_phs),"mean_A_phs_w:",numpy.mean(Avrg_Y_phs))
     #print("std_A_phs_w_mean:",numpy.std(Y_phs_w_mean),"mean_A_phs_w_mean:",numpy.mean(Y_phs_w_mean))
-    #print("std_A_all_w:",numpy.std(A_all_w),"mean_A_all_w:",numpy.mean(A_all_w))
-    print("std_A_all_w_mean:",numpy.std(Y_all_w_mean),"mean_A_all_w_mean:",numpy.mean(Y_all_w_mean))
-   
+    print("std_A_all_w:",numpy.std(Avrg_Y_all),"mean_A_all_w:",numpy.mean(Avrg_Y_all))
+    #print("std_A_all_w_mean:",numpy.std(Y_all_w_mean),"mean_A_all_w_mean:",numpy.mean(Y_all_w_mean))
+    
     #plt.hist(Y_qnt_mean, bins)
     #plt.show()
